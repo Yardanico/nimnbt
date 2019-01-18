4a037433b385bc65364e2376794e5139b1059e52
Yardanico <21169548+Yardanico@users.noreply.github.com>
2019-01-19 01:41:10 +0300

    Use sizeof() instead of passing size explicitly. Some style fixes / comments

--- src/nimnbt.nim
+++ src/nimnbt.nim
@@ -1,12 +1,15 @@
+# A Nim library for parsing NBT file format used in Minecraft
+# https://wiki.vg/NBT was used as a reference to the format
+
 import streams, endians, tables
-import zip/zlib # Wrapper for the libzip library
+import zip/zlib               # Wrapper for the libzip library
 
 
 type
   TagKind* = enum
-    End, Byte, Short, Int, Long, Float, Double, ByteArray, 
+    End, Byte, Short, Int, Long, Float, Double, ByteArray,
     String, List, Compound, IntArray, LongArray
-  
+
   Tag* = object
     name*: string
     case kind*: TagKind
@@ -16,11 +19,11 @@ type
     of Int: intVal*: int32
     of Long: longVal*: int64
     of Float: floatVal*: float32
-    of Double: doubleVal*: float64  # XXX: this might be incorrect
+    of Double: doubleVal*: float64 # XXX: this might be incorrect
     of ByteArray: bytes*: seq[int8]
     of String: str*: string
     of List:
-      typ*: TagKind  ## List type 
+      typ*: TagKind           ## List type
       values*: seq[Tag]
     of Compound: compound*: Table[string, Tag]
     of IntArray: ints: seq[int32]
@@ -29,23 +32,27 @@ type
 
 const isLittle = system.cpuEndian == littleEndian
 
-template endian(x: untyped, size: static[int]): untyped =
+template endian(x: untyped): untyped =
   ## A convenient template to swap endianness of variables to big endian
-  var val = x
-  when isLittle:
-    when size == 16:
-      bigEndian16(addr val, addr val)
-    elif size == 32:
-      bigEndian32(addr val, addr val)
-    elif size == 64:
-      bigEndian64(addr val, addr val)
-    else: discard
-  val
+  # When we are on big-endian platform, we don't need to change endianness
+  when not isLittle:
+    x
+  else:
+    var val = x
+    when isLittle:
+      when sizeof(x) == 2:
+        bigEndian16(addr val, addr val)
+      elif sizeof(x) == 4:
+        bigEndian32(addr val, addr val)
+      elif sizeof(x) == 8:
+        bigEndian64(addr val, addr val)
+      else: discard
+    val
 
 
-proc readString(s: Stream): string {.inline.} = 
+proc readString(s: Stream): string {.inline.} =
   # Thankfully strings in NBT are UTF-8 (same as in Nim)
-  let len = endian(s.readUint16(), 16)
+  let len = endian(s.readUint16())
   if len == 0: ""
   else: s.readStr(int(len))
 
@@ -60,18 +67,18 @@ proc parseNbtInternal(s: Stream, tagKind = End, parseName = true): Tag =
   of End: return
   of Byte:
     result.byteVal = s.readInt8()
-  of Short: 
-    result.shortVal = endian(s.readInt16(), 16)
+  of Short:
+    result.shortVal = endian(s.readInt16())
   of Int:
-    result.intVal = endian(s.readInt32(), 32)
+    result.intVal = endian(s.readInt32())
   of Long:
-    result.longVal = endian(s.readInt64(), 64)
+    result.longVal = endian(s.readInt64())
   of Float:
-    result.floatVal = endian(s.readFloat32(), 32)
+    result.floatVal = endian(s.readFloat32())
   of Double:
-    result.doubleVal = endian(s.readFloat64(), 64)
+    result.doubleVal = endian(s.readFloat64())
   of ByteArray:
-    let size = endian(s.readInt32(), 32)
+    let size = endian(s.readInt32())
     var i = 0
     result.bytes = newSeqOfCap[int8](size)
 
@@ -82,7 +89,7 @@ proc parseNbtInternal(s: Stream, tagKind = End, parseName = true): Tag =
     result.str = s.readString()
   of List:
     result.typ = TagKind(s.readUint8())
-    let size = endian(s.readInt32(), 32)
+    let size = endian(s.readInt32())
     var i = 0
     result.values = newSeqOfCap[Tag](size)
 
@@ -97,42 +104,43 @@ proc parseNbtInternal(s: Stream, tagKind = End, parseName = true): Tag =
       if nextTag.kind == End: break
       result.compound[nextTag.name] = nextTag
   of IntArray:
-    let size = endian(s.readInt32(), 32)
+    let size = endian(s.readInt32())
     var i = 0
     result.ints = newSeqOfCap[int32](size)
 
     while i < size:
-      result.ints.add(endian(s.readInt32(), 32))
+      result.ints.add(endian(s.readInt32()))
       inc(i)
   of LongArray:
-    let size = endian(s.readInt32(), 32)
+    let size = endian(s.readInt32())
     var i = 0
     result.longs = newSeqOfCap[int64](size)
 
     while i < size:
-      result.longs.add(endian(s.readInt64(), 64))
+      result.longs.add(endian(s.readInt64()))
       inc(i)
 
 
-proc decompress(s: string): StringStream {.inline.} = 
+proc decompress(s: string): StringStream =
+  # Some ugly checks for different types of compression
   result = newStringStream(
-    if s[0..2] == "\x1f\x8b\x08": 
-      uncompress(s, len(s), GZIP_STREAM)
-    elif s[0] == 'x' and s[1] in {'\x01', '\x9c', '\xDA'}:
-      uncompress(s, len(s), ZLIB_STREAM)
-    else:
-      s
+    if s[0..2] == "\x1f\x8b\x08":
+    uncompress(s, len(s), GZIP_STREAM)
+  elif s[0] == 'x' and s[1] in {'\x01', '\x9c', '\xDA'}:
+    uncompress(s, len(s), ZLIB_STREAM)
+  else:
+    s
   )
 
 
-proc parseNbt*(s: Stream): Tag {.inline.} = 
+proc parseNbt*(s: Stream): Tag =
   ## Parses NBT data structure from the stream *s*.
   ##
   ## *s* may be compressed via gzip/zlib
   result = parseNbtInternal(s)
 
 
-proc parseNbtFile*(filename: string): Tag {.inline.} = 
+proc parseNbtFile*(filename: string): Tag =
   ## Parses NBT data structure from the file *filename*.
   ##
   ## File may be compressed via gzip/zlib
@@ -140,18 +148,18 @@ proc parseNbtFile*(filename: string): Tag {.inline.} =
   result = parseNbtInternal(decompress(data))
 
 
-proc parseNbt*(s: string): Tag {.inline.} = 
+proc parseNbt*(s: string): Tag =
   ## Parses NBT data structure from the string *s*
   ##
   ## *s* may be compressed via gzip/zlib
   result = parseNbtInternal(decompress(s))
 
-proc `[]`*(t: Tag, name: string): Tag {.inline.} = 
+proc `[]`*(t: Tag, name: string): Tag =
   ## A convenient proc to access tag in the compound
   t.compound[name]
 
 
-proc len*(t: Tag): int {.inline.} = 
+proc len*(t: Tag): int =
   ## A convenient proc to access length of some container types
   ## Works for ByteArray, String, List, Compound, IntArray, LongArray
   case t.kind
--- tests/bigtest.nim
+++ tests/bigtest.nim
@@ -13,7 +13,7 @@ suite "bigtest.nbt":
     check(tag.kind == Compound)
     check(tag.name == "Level")
     check(tag.len == 11)
-  
+
   test "nested compound test":
     let nested = tag["nested compound test"]
     check(nested.len == 2)
@@ -27,16 +27,16 @@ suite "bigtest.nbt":
     check(ham.len == 2)
     check(ham["name"].str == "Hampus")
     check(ham["value"].floatVal == 0.75)
-  
+
   test "intTest":
     check(tag["intTest"].intVal == 2147483647)
-  
+
   test "byteTest":
     check(tag["byteTest"].byteVal == 127)
-  
+
   test "stringTest":
     check(tag["stringTest"].str == "HELLO WORLD THIS IS A TEST STRING \xc3\x85\xc3\x84\xc3\x96!")
-  
+
   test "listTest (long)":
     let list = tag["listTest (long)"]
     check(list.len == 5)
@@ -44,16 +44,16 @@ suite "bigtest.nbt":
     for value in list.values:
       values.add(value.longVal)
     check(values == @[11'i64, 12, 13, 14, 15])
-  
+
   test "doubleTest":
     check(tag["doubleTest"].doubleVal == 0.49312871321823148'f64)
-  
+
   test "floatTest":
     check(tag["floatTest"].floatVal == 0.49823147058486938'f32)
-  
+
   test "longTest":
     check(tag["longTest"].longVal == 9223372036854775807)
-  
+
   test "listTest (compound)":
     let list = tag["listTest (compound)"]
     check(list.len == 2)
@@ -61,12 +61,12 @@ suite "bigtest.nbt":
       check(value.len == 2)
       check(value["created-on"].longVal == 1264099775885'i64)
       check(value["name"].str == "Compound tag #" & $i)
-  
+
   test "byteArrayTest":
     const name = "byteArrayTest (the first 1000 values of (n*n*255+n*7)%100, starting with n=0 (0, 62, 34, 16, 8, ...))"
     for n in 0 ..< 1000:
       check(tag[name].bytes[n] == (n * n * 255 + n * 7) mod 100)
-  
+
   test "shortTest":
     let shortTag = tag["shortTest"]
     check(shortTag.shortVal == 32767)
